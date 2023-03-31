cc3c2093ca1e390926941569da6f1405c00f9d90
H-Hassan81 <129502272+H-Hassan81@users.noreply.github.com>
2023-03-31 10:57:28 +0100

    Update streamlit_app.py
    moved header position

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
+
 streamlit.title("My Parent's new health diner")
+
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled Free Range Egg')
