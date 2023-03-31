1500c5774ede0701087f2238512165b800f77900
H-Hassan81 <129502272+H-Hassan81@users.noreply.github.com>
2023-03-31 10:56:57 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfast Menu'
+streamlit.header('Breakfast Menu')
 streamlit.title("My Parent's new health diner")
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
