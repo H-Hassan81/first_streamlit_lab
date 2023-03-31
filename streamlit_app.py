1e49932692e9887f7535f9fe67d40b2996cd54b9
H-Hassan81 <129502272+H-Hassan81@users.noreply.github.com>
2023-03-31 10:55:26 +0100

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title("My Parent's new health diner")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free Range Egg')
